d23c6db08cf4e4d3f777c175b4ded8bda449a91b
anshumansahu620 <anshu2322sahu@gmail.com>
2025-12-16 11:35:28 +0530

    models and db schema added

---
+++ sharing/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+#from django.contrib.gis.db import
+from django.core.validators import MinValueValidator,MaxValueValidator
+
+import uuid
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    phone=models.CharField(unique=True)
+    email=models.CharField(unique=True)
+    
+
+
+
+
+    
+class   HelpRequest(models.Model):
+    request_by=models.ForeignKey(User,on_delete=models.CASCADE)
+    address=models.CharField(max_length=2000)
+    pin_code=models.CharField(max_length=255)
+    country=models.CharField(max_length=255)
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        help_text="Latitude in decimal degrees"
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        help_text="Longitude in decimal degrees"
+    )
+
+    accuracy_meters = models.FloatField(null=True, blank=True)
+    altitude_meters = models.FloatField(null=True, blank=True)
+    
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
